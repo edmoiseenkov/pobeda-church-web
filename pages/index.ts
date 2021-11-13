51d35069d2dac97d0a95eaac05969336fb53970c
Eduard <edmoiseenkov@gmail.com>
2021-11-13 23:43:10 +0200

    turned off preview mode

--- pages/index.ts
+++ pages/index.ts
@@ -1,2 +1,1 @@
-// export { Home as default, getStaticProps } from '@app/pages/home';
-export { Preview as default } from '@app/pages/preview';
+export { Home as default, getStaticProps } from '@app/pages/home';
